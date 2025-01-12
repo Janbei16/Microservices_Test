61d870090ffb4f6280571cfc01c275857d192357
Janbei16 <jannisbeichler@gmail.com>
2025-01-12 14:42:52 +0100

    Project

---
+++ microservice_Test/src/test/java/com/bbw/microservice_Test/MicroserviceTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bbw.microservice_Test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
