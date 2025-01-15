0fef63585a762ce7a73abc291e130d53f35a33a9
Janbei16 <jannisbeichler@gmail.com>
2025-01-15 20:26:23 +0100

    add endpoints

--- microservice_Test/src/main/java/com/bbw/microservice_Test/ProductService.java
+++ microservice_Test/src/main/java/com/bbw/microservice_Test/ProductService.java
@@ -1,5 +1,3 @@
-package com.bbw.microservice_Test;
-
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,7 +12,28 @@ public class ProductService {
         return productRepository.findAll();
     }
 
-    public void saveProduct(Product product) {
-        productRepository.save(product);
+    public Product getProductById(Long id) {
+        return productRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Product not found with id: " + id));
+    }
+
+    public Product saveProduct(Product product) {
+        return productRepository.save(product);
+    }
+
+    public Product updateProduct(Long id, Product updatedProduct) {
+        Product existingProduct = productRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Product not found with id: " + id));
+
+        existingProduct.setName(updatedProduct.getName());
+        existingProduct.setDescription(updatedProduct.getDescription());
+        existingProduct.setPrice(updatedProduct.getPrice());
+        existingProduct.setStock(updatedProduct.getStock(0));
+
+        return productRepository.save(existingProduct);
+    }
+
+    public void deleteProduct(Long id) {
+        productRepository.deleteById(id);
     }
 }
--- microservice_Test/src/main/java/com/bbw/microservice_Test/ShopController.java
+++ microservice_Test/src/main/java/com/bbw/microservice_Test/ShopController.java
@@ -1,20 +1,48 @@
-package com.bbw.microservice_Test;
-
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/products")
 public class ShopController {
-    @Autowired 
+    @Autowired
     private ProductService productService;
 
+    // GET all products
     @GetMapping
     public List<Product> getAllProducts() {
         return productService.listAll();
+    }
+
+    // GET a single product by ID
+    @GetMapping("/{id}")
+    public Product getProductById(@PathVariable Long id) {
+        return productService.getProductById(id);
+    }
+
+    // POST to add a new product
+    @PostMapping
+    public void addProduct(@RequestBody Product product) {
+        productService.saveProduct(product);
+    }
+
+    // PUT to update a product
+    @PutMapping("/{id}")
+    public Product updateProduct(@PathVariable Long id, @RequestBody Product product) {
+        return productService.updateProduct(id, product);
+    }
+
+    // DELETE to remove a product
+    @DeleteMapping("/{id}")
+    public void deleteProduct(@PathVariable Long id) {
+        productService.deleteProduct(id);
     }
 }
